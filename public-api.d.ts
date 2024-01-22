d77a912c7fe783d1bc12f60f8be87b0706413058
Nupin <143161837+npnsap@users.noreply.github.com>
2024-01-22 07:35:59 +0000

    Moved dist folder files to root

---
+++ public-api.d.ts
@@ -0,0 +1,7 @@
+export * from './lib/caret-coords';
+export * from './lib/mention-utils';
+export * from './lib/mention-config';
+export * from './lib/mention-list.component';
+export * from './lib/mention.directive';
+export * from './lib/mention.module';
+//# sourceMappingURL=public-api.d.ts.map
